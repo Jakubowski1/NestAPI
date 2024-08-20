5f0a94306edf058bba171232cb0da4da7c69b297
Mati <jakubowski.projects@gmail.com>
2024-08-20 14:40:20 +0200

    Working on deployment with render

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import * as cookieParser from 'cookie-parser';
@@ -11,26 +11,28 @@ import JwtCookieMiddleware from './auth/jwt-cookie.middleware';
 
 @Module({
   imports: [
+    // Load environment variables globally
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
+    // Configure TypeORM to use DATABASE_URL from environment variables
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('PG_HOST'),
-        port: configService.get<number>('PG_PORT'),
-        username: configService.get<string>('PG_USER'),
-        password: configService.get<string>('PG_PASSWORD'),
-        database: configService.get<string>('PG_DB'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migrations/**/*.ts'],
-        synchronize: configService.get<boolean>('TYPEORM_SYNC', false),
+        url: configService.get<string>('DATABASE_URL'), // Use DATABASE_URL from environment variables
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // Adjust entity path according to your structure
+        migrations: [__dirname + '/migrations/**/*.ts'], // Adjust migrations path if necessary
+        synchronize: configService.get<boolean>('TYPEORM_SYNC', false), // Recommended to disable in production
+        ssl: {
+          rejectUnauthorized: false,
+        }, // Optional: for SSL configuration, depending on your database setup
       }),
       inject: [ConfigService],
     }),
 
+    // Other feature modules
     UsersModule,
     PostsModule,
     AuthModule,

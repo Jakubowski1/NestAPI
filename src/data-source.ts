503f8024631882cd29316d12b8bb7d918c919800
Mati <jakubowski.projects@gmail.com>
2024-08-20 14:17:32 +0200

    Working on deployment

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,12 +7,17 @@ const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
+
+  // If DATABASE_URL is available, use it. Otherwise, use individual configs.
+  url: configService.get('DATABASE_URL') || undefined,
+
   host: configService.get('DATABASE_HOST', 'localhost'),
   port: configService.get<number>('DATABASE_PORT', 5432),
   username: configService.get('DATABASE_USERNAME', 'postgres'),
   password: configService.get('DATABASE_PASSWORD', 'admin'),
   database: configService.get('DATABASE_NAME', 'postgres'),
+
   entities: [User, Post],
-  synchronize: false,
+  synchronize: false,  // Should be false in production to avoid unintentional schema changes
   migrations: ['src/migrations/*.ts'],
 });
